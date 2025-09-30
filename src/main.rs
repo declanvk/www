0a7139c3e0b81d3e8a7aaca7edf09c32b9255638
Declan Kelly <dkelly.home@gmail.com>
2025-09-29 22:44:42 -0700

    Add argument parsing and logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use argh::FromArgs;
+use tracing::{debug, info};
+
+/// A blazing fast static site generator.
+#[derive(FromArgs, Debug)]
+struct Cli {
+    /// be verbose
+    #[argh(switch, short = 'v')]
+    verbose: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli: Cli = argh::from_env();
+
+    let log_level = if cli.verbose {
+        tracing::Level::DEBUG
+    } else {
+        tracing::Level::INFO
+    };
+
+    tracing_subscriber::fmt().with_max_level(log_level).init();
+
+    info!("Starting up...");
+    debug!("This is a debug message and will only show in verbose mode.");
+
+    info!("All done!");
 }
