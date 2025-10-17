b0da476c74baf0ea5c33b1b893691e186e6e2afa
Declan Kelly <dkelly.home@gmail.com>
2025-10-17 14:13:24 -0700

    Factor out djot processing into functions

--- src/build/djot.rs
+++ src/build/djot.rs
@@ -23,6 +23,84 @@ fn collect_strings(events: &[Event<'_>]) -> (String, usize) {
     (content, num_str_events)
 }
 
+fn extract_frontmatter(
+    metadata: &mut MetadataContainer,
+    slug: &ContentSlug,
+    events: &mut Vec<Event<'_>>,
+) -> anyhow::Result<()> {
+    if !matches!(
+        &events[..],
+        [Event::Start(Container::RawBlock { format: "json" }, _), ..]
+    ) {
+        debug!("Missing json raw block start, skipping frontmatter");
+        return Ok(());
+    }
+
+    // We know at this point that we're in a raw json block, so we'll expect the
+    // next event(s) to be `Str`
+    let (frontmatter, num_str_events) = collect_strings(&events[1..]);
+
+    // Also need the block to terminate
+    if !matches!(
+        &events[1 + num_str_events],
+        Event::End(Container::RawBlock { format: "json" })
+    ) {
+        debug!("Missing raw block ending, skipping frontmatter");
+        return Ok(());
+    }
+
+    let frontmatter: Frontmatter =
+        serde_json::from_str(&frontmatter).context("failed to parse frontmatter")?;
+
+    debug!(?frontmatter, "Parsed frontmatter from djot file");
+
+    if let Some(map) = frontmatter.0.as_object()
+        && let Some(Value::String(bibliography_field)) = map.get("bibliography")
+    {
+        metadata[slug].bibliography_file = Some(bibliography_field.clone());
+    }
+    metadata[slug].frontmatter = Some(frontmatter);
+
+    // Remove events from the start
+    events.drain(..(1 + num_str_events + 1));
+
+    Ok(())
+}
+
+fn find_title(
+    metadata: &mut MetadataContainer,
+    slug: &ContentSlug,
+    events: &[Event<'_>],
+) -> anyhow::Result<()> {
+    let mut events_it = events
+        .iter()
+        .enumerate()
+        .filter(|(_, e)| matches!(e, Event::Start(Container::Heading { level: 1, .. }, _)));
+
+    let Some((title_offset, _)) = events_it.next() else {
+        debug!("Missing page title start, skipping");
+        return Ok(());
+    };
+
+    if events_it.next().is_some() {
+        bail!("Found multiple level 1 headers in the same document");
+    }
+
+    let (title, num_str_events) = collect_strings(&events[(title_offset + 1)..]);
+
+    if !matches!(
+        &events.get(title_offset + num_str_events + 1),
+        Some(Event::End(Container::Heading { level: 1, .. }))
+    ) {
+        debug!("Missing page title end, skipping");
+        return Ok(());
+    }
+
+    metadata[slug].title = Some(title);
+
+    Ok(())
+}
+
 #[tracing::instrument(skip_all)]
 pub fn render(
     input: &BuildFile,
@@ -32,71 +110,9 @@ pub fn render(
 ) -> anyhow::Result<String> {
     let mut events = jotdown::Parser::new(content).collect::<Vec<_>>();
 
-    'extract_frontmatter: {
-        if !matches!(
-            &events[..],
-            [Event::Start(Container::RawBlock { format: "json" }, _), ..]
-        ) {
-            debug!("Missing json raw block start, skipping frontmatter");
-            break 'extract_frontmatter;
-        }
+    extract_frontmatter(metadata, slug, &mut events).context("extracting frontmatter")?;
 
-        // We know at this point that we're in a raw json block, so we'll expect the
-        // next event(s) to be `Str`
-        let (frontmatter, num_str_events) = collect_strings(&events[1..]);
-
-        // Also need the block to terminate
-        if !matches!(
-            &events[1 + num_str_events],
-            Event::End(Container::RawBlock { format: "json" })
-        ) {
-            debug!("Missing raw block ending, skipping frontmatter");
-            break 'extract_frontmatter;
-        }
-
-        let frontmatter: Frontmatter =
-            serde_json::from_str(&frontmatter).context("failed to parse frontmatter")?;
-
-        debug!(?frontmatter, "Parsed frontmatter from djot file");
-
-        if let Some(map) = frontmatter.0.as_object()
-            && let Some(Value::String(bibliography_field)) = map.get("bibliography")
-        {
-            metadata[slug].bibliography_file = Some(bibliography_field.clone());
-        }
-        metadata[slug].frontmatter = Some(frontmatter);
-
-        // Remove events from the start
-        events.drain(..(1 + num_str_events + 1));
-    }
-
-    'find_title: {
-        let mut events_it = events
-            .iter()
-            .enumerate()
-            .filter(|(_, e)| matches!(e, Event::Start(Container::Heading { level: 1, .. }, _)));
-
-        let Some((title_offset, _)) = events_it.next() else {
-            debug!("Missing page title start, skipping");
-            break 'find_title;
-        };
-
-        if events_it.next().is_some() {
-            bail!("Found multiple level 1 headers in the same document");
-        }
-
-        let (title, num_str_events) = collect_strings(&events[(title_offset + 1)..]);
-
-        if !matches!(
-            &events.get(title_offset + num_str_events + 1),
-            Some(Event::End(Container::Heading { level: 1, .. }))
-        ) {
-            debug!("Missing page title end, skipping");
-            break 'find_title;
-        }
-
-        metadata[slug].title = Some(title);
-    }
+    find_title(metadata, slug, &events).context("finding page title")?;
 
     biblatex::handle_references(input, metadata, slug, &mut events)
         .context("parsing out citations and inserting reference")?;
